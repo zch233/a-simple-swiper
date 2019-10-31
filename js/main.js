1b2dc99faf9979a2037d8634577e849bf37b5487
zhangchao <529743595@qq.com>
2019-10-31 20:08:18 +0800

    通过js切换enter leave current 三中状态来控制轮播

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function Swiper(option) {
+  this.option = option
+  this.init()
+}
+
+Swiper.prototype = {
+  index: 0,
+  init() {
+    $('.images>img').eq(this.slideIndex(this.index)).addClass('current').siblings().addClass('enter')
+    $('.images>img').css({ 'transition-duration': this.duration + 's' })
+    this.xxx()
+  },
+  xxx() {
+    const delay = this.option.delay
+    const slide = () => {
+      $('.images>img').eq(this.slideIndex(this.index)).addClass('leave').one('transitionend', function() {
+        $(this).removeClass('current leave').addClass('enter')
+      })
+      $('.images>img').eq(this.slideIndex(this.index + 1)).removeClass('enter leave').addClass('current')
+      this.index += 1
+      setTimeout(slide, delay)
+    }
+    setTimeout(slide, delay);
+  },
+  slideIndex(index) {
+    return index % 3
+  }
+}
+
+new Swiper({ duration: 1, delay: 1000 })
